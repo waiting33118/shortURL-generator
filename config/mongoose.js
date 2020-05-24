7cc3459b4e4948ba599ca3df787c628ba58e1768
Tony <waiting33118@gmail.com>
2020-05-24 16:53:11 +0800

    feat: create mongodb config

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/urlDB'
+
+mongoose.connect(MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+
+db.on('error', () => console.log('connection error!'))
+db.once('open', () => console.log('mongodb connected!'))
+
+module.exports = db
