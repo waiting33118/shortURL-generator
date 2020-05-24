418bdc5e8390ceb943054daf192435b37dc1da14
Tony <waiting33118@gmail.com>
2020-05-24 23:13:23 +0800

    feat: create homePage Form function(POST)

--- routes/modules/homePage.js
+++ routes/modules/homePage.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const Url = require('../../models/URL')
+const generator = require('../../public/javascripts/shortUrlGenerator')
 
 router.get('/', (req, res) => {
-	res.render('homePage')
+  res.render('homePage')
 })
 
+router.post('/', (req, res) => {
+  const inputUrl = req.body.url
+  const hostname = req.hostname
+
+  Url.findOne({ url: `${inputUrl}` })
+    .lean()
+    .then((url) => {
+      console.log(url)
+      if (url) return res.render('resultPage', { url, hostname })
+      const newShortUrl = generator()
+      Url.create({ url: `${inputUrl}`, sUrl: `${newShortUrl}` })
+        .then(
+          res.render('newResultPage', {
+            url: inputUrl,
+            sUrl: newShortUrl,
+            hostname
+          })
+        )
+        .catch((err) => console.log(err))
+    })
+    .catch((err) => console.log(err))
+})
 module.exports = router
