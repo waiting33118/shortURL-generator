1589a9f8bf7ae0b1c421b6b19d15d9e9192ebb71
Tony <waiting33118@gmail.com>
2020-05-25 13:49:55 +0800

    feat: add url redirect function

--- routes/modules/homePage.js
+++ routes/modules/homePage.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const Url = require('../../models/URL')
 const generator = require('../../public/javascripts/shortUrlGenerator')
 
+// 渲染主頁面
 router.get('/', (req, res) => {
   res.render('homePage')
 })
 
+// 產生短網址
 router.post('/', (req, res) => {
   const inputUrl = req.body.url
   const hostname = req.hostname
@@ -14,18 +16,33 @@ router.post('/', (req, res) => {
   Url.findOne({ url: `${inputUrl}` })
     .lean()
     .then((url) => {
-      console.log(url)
-      if (url) return res.render('resultPage', { url, hostname })
+      if (url) return res.render('resultPage', { url })
       const newShortUrl = generator()
-      Url.create({ url: `${inputUrl}`, sUrl: `${newShortUrl}` })
+      const shortUrlLink = `${hostname}/${newShortUrl}`
+      Url.create({
+        url: `${inputUrl}`,
+        shortUrlLink,
+        sUrl: newShortUrl
+      })
         .then(
           res.render('newResultPage', {
             url: inputUrl,
-            sUrl: newShortUrl,
-            hostname
+            shortUrlLink
           })
         )
         .catch((err) => console.log(err))
+    })
+    .catch((err) => console.log(err))
+})
+
+// 短網址轉址
+router.get('/:_sUrl', (req, res) => {
+  const sUrl = req.params._sUrl
+  Url.findOne({ sUrl: `${sUrl}` })
+    .lean()
+    .then((url) => {
+      if (url) return res.redirect(url.url)
+      return res.redirect(404)
     })
     .catch((err) => console.log(err))
 })
